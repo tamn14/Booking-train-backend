fb2e9d986c3432c7fc507b61aee8b073cb106005
Nguyen Chi Tam <tamn0443@gmail.com>
2025-05-31 12:44:33 +0700

    init project

---
+++ src/main/java/com/example/booking_train_backend/BookingTrainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking_train_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingTrainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingTrainBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booking_train_backend/BookingTrainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking_train_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTrainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
