f84b8c766009e5d7c4e142a10c0f27626e5bc712
Nguyen Chi Tam <tamn0443@gmail.com>
2025-07-01 13:32:02 +0700

    debug keycloak

--- src/main/java/com/example/booking_train_backend/Config/ApplicationConfig.java
+++ src/main/java/com/example/booking_train_backend/Config/ApplicationConfig.java
@@ -9,7 +9,9 @@ import com.example.booking_train_backend.Entity.Users;
 import com.example.booking_train_backend.Properties.IdpProperties;
 import com.example.booking_train_backend.Repo.IdentityProviderRepo;
 import com.example.booking_train_backend.Repo.UsersRepo;
+import com.example.booking_train_backend.Service.ServiceInterface.KeycloakClientTokenService;
 import com.example.booking_train_backend.Service.ServiceInterface.UserService;
+import com.example.booking_train_backend.Util.Extract;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -23,11 +25,58 @@ import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE , makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class ApplicationConfig {
+    @Bean
+    ApplicationRunner applicationRunner(UsersRepo usersRepo,
+                                        IdentityProviderRepo identityProviderRepo,
+                                        KeycloakClientTokenService keycloakClientTokenService,
+                                        IdpProperties idpProperties,
+                                        Extract extract) {
+        return args -> {
+            if (!usersRepo.existsByUserName("admin")) {
 
+                // lay access Token de co the goi API keycloak tao nguoi dung
+                var accessToken = keycloakClientTokenService.getAccessToken();
+                var creationResponse = identityProviderRepo.createUser(
+                        idpProperties.getRealm(),
+                        "Bearer " + accessToken,
+                        UserCreationParam.builder()
+                                .username("admin")
+                                .enabled(true)
+                                .emailVerified(false)
+                                .credentials(List.of(Credential.builder()
+                                        .type("password")
+                                        .value("admin")
+                                        .temporary(false)
+                                        .build()))
+                                .build());
+                String userKeycloakId = extract.extractUserId(creationResponse);
 
+                // gan role cho user vua tao
+                var role = identityProviderRepo.getRealmRoleByName(idpProperties.getRealm(),
+                        "Bearer " + accessToken,
+                        "ADMIN"
+                );
+                identityProviderRepo.assignRealmRolesToUser(
+                        idpProperties.getRealm(),
+                        "Bearer " + accessToken,
+                        userKeycloakId,
+                        List.of(role)
+                );
 
+                // luu thong tin vao db
+                Users users = Users.builder()
+                        .userName("admin")
+                        .email("Admin@gmail.com")
+                        .userKeycloakId(userKeycloakId)
+                        .build();
+                // luu user vao db ( vi day la he thong booking nen co the hieu khi tao user moi la tao passenger moi)
+                usersRepo.save(users);
+                
+            }
 
+        };
+    }
 }
--- src/main/java/com/example/booking_train_backend/Entity/Users.java
+++ src/main/java/com/example/booking_train_backend/Entity/Users.java
@@ -1,10 +1,7 @@
 package com.example.booking_train_backend.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -18,6 +15,7 @@ import java.util.List;
 @Table(name = "passenger" , uniqueConstraints = {
         @UniqueConstraint(columnNames = "email")
 })
+@Builder
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
